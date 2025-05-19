e6438d0d27450184df19010f2ea282b419d3dcd4
Logesh018 <vlogesh018@gmail.com>
2025-05-19 15:46:55 +0530

    Student_Info Added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,7 @@ import "./Footer.css"
 const Footer = () => {
   return (
     <div className='footer'>
-      <p>Copyright © {new Date().getFullYear()}. All rights reserved. Created and Developed by Logesh.</p>
+      <p>Copyright © {new Date().getFullYear()}. All rights reserved. Created and Maintained by Logesh.</p>
       <ul>
         <li>Terms of Services</li>
         <li>Privacy Policy</li>
